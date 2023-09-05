641f0737adc5a9896da9d6d7cd7bb9a49ab8027b
florbert <habimanaflorbert@gmail.com>
2023-09-05 13:59:01 +0200

    chnage

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 # 3rd party imports
 from rest_framework import routers
-from home.views import (PatientViewset,HouseHoldViewset,MalnutritionViewset,ContraceptionViewset)
+from home.views import (DocumentationViewset,PatientViewset,HouseHoldViewset,MalnutritionViewset,ContraceptionViewset)
 
 
 route = routers.DefaultRouter()
@@ -10,6 +10,7 @@ route.register('patient', PatientViewset)
 route.register('family',HouseHoldViewset)
 route.register('malnutrition',MalnutritionViewset)
 route.register('contraception',ContraceptionViewset)
+route.register('document',DocumentationViewset)
 
 urlpatterns = [
     path('', include(route.urls)),
